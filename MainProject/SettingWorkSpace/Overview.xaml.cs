86f90def7c3480c42450dab63ffe8b36bb5062c4
Đoàn Ngọc Lãm <19521737@gm.uit.edu.vn>
2021-06-06 19:09:21 +0700

    VM_Setting

--- MainProject/SettingWorkSpace/Overview.xaml.cs
+++ MainProject/SettingWorkSpace/Overview.xaml.cs
@@ -20,9 +20,70 @@ namespace MainProject.SettingWorkSpace
     /// </summary>
     public partial class Overview : Page
     {
+        #region fields
+
+        private String _nameStore;
+        private String _numberPhone;
+        private String _address;
+        
+        #endregion
+
         public Overview()
         {
             InitializeComponent();
+        }
+
+        #region propertities
+        public String NameStore
+        {
+            get => _nameStore;
+            set
+            {
+                _nameStore = value;
+            }
+        }
+
+        public String NumberPhone
+        {
+            get => _numberPhone;
+            set
+            {
+                _numberPhone = value;
+            }
+        }
+
+        public String Adress
+        {
+            get => _address;
+            set
+            {
+                _address = value;
+            }
+        }
+
+        #endregion
+
+        private void btn_SaveName_Click(object sender, RoutedEventArgs e)
+        {
+            txb_Name.IsEnabled = true;
+            txb_Name.Focusable = true;
+        }
+
+        private void btn_SavePhone_Click(object sender, RoutedEventArgs e)
+        {
+            txb_numberPhone.IsEnabled = true;
+            txb_numberPhone.Focusable = true;
+        }
+
+        private void btn_SaveAddress_Click(object sender, RoutedEventArgs e)
+        {
+            txb_address.IsEnabled = true;
+            txb_address.Focusable = true;
+        }
+
+        private void btn_NumberFloor_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
