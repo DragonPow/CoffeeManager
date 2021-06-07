9acb270d335061ba7b31efeb9cff936e1601694f
UIT.19520266 <19520266@gm.uit.edu.vn>
2021-06-07 23:43:50 +0700

    complete bill

--- MainProject/MainWorkSpace/Bill/BillView.xaml.cs
+++ MainProject/MainWorkSpace/Bill/BillView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,35 @@ namespace MainProject.MainWorkSpace.Bill
     /// <summary>
     /// Interaction logic for BillView.xaml
     /// </summary>
-    public partial class BillView : Window
+    public partial class BillView : Window, INotifyPropertyChanged
     {
         public BillView()
         {
             InitializeComponent();
+            txtMoneyCustomer.PreviewTextInput += TxtMoneyCustomer_PreviewTextInput;
+        }
+
+        private void TxtMoneyCustomer_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Any(c => !char.IsDigit(c))) e.Handled = true;
+            else e.Handled = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int ErrorCount { get; set; }
+        public bool IsValid { get => ErrorCount < 1; }
+
+        private void Validation_Error(object sender, ValidationErrorEventArgs e)
+        {
+            if (e.Action == ValidationErrorEventAction.Added)
+            {
+                ErrorCount++;
+            }
+            else
+            {
+                ErrorCount--;
+            }
         }
     }
 }
