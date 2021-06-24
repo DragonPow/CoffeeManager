0da4f757fa7fcd0a6383f06a13d647283f25986e
UIT.19520266 <19520266@gm.uit.edu.vn>
2021-06-24 23:52:24 +0700

    [ADD BILL_VIEW_READONLY] - change billVM
    - add Messagebox Manager
    - add view bill_readonly

--- MainProject/HistoryWorkSpace/HistoryListItemView.xaml.cs
+++ MainProject/HistoryWorkSpace/HistoryListItemView.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MainProject.MainWorkSpace.Bill;
+using MainProject.Model;
+using MainProject.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,9 +29,27 @@ namespace MainProject.HistoryWorkSpace
             InitializeComponent();
         }
 
-        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-
+            //BillView_readonly view = new BillView_readonly();
+            //BILL overviewbill = ((ViewModel.HistoryViewModel)DataContext).CurrentBill;
+            //BILL bill;
+            //using (mainEntities db = new mainEntities())
+            //{
+            //    bill = db.BILLs.Where(b => b.ID == overviewbill.ID).Include("DETAILBILLs").First();
+            //    foreach (DETAILBILL detail in bill.DETAILBILLs)
+            //    {
+            //        var pro = (from p in db.PRODUCTs.Where(i => i.ID == detail.ID_Product).DefaultIfEmpty()
+            //                  select new
+            //                  {
+            //                      Name = p.Name
+            //                  }).FirstOrDefault();
+            //        detail.PRODUCT = new PRODUCT();
+            //        detail.PRODUCT.Name = pro.Name;
+            //    }
+            //}
+            //view.DataContext = bill;
+            //view.Show();
         }
     }
 }
---
+++ MainProject/MainWorkSpace/Bill/BillView_readonly.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MainProject.MainWorkSpace.Bill
+{
+    /// <summary>
+    /// Interaction logic for BillView.xaml
+    /// </summary>
+    public partial class BillView_readonly : Window, INotifyPropertyChanged
+    {
+        public BillView_readonly()
+        {
+            InitializeComponent();
+        }
+
+        public long Refund
+        {
+            get
+            {
+                if (DataContext != null)
+                {
+                    Model.BILL b = (Model.BILL)DataContext;
+                    return b.MoneyCustomer - b.TotalPrice;
+                }
+                return 0;
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
